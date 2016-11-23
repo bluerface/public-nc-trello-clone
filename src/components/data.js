5eadf1284f31eff5033fc25be8418000336a87c6
bluerface <fallingsilkworm@gmail.com>
2016-11-23 15:59:07 +0000

    Add extra default data

--- src/components/data.js
+++ src/components/data.js
@@ -9,8 +9,12 @@ const board = {
 board.lists.push(new ListObject('My first list'));
 board.lists.push(new ListObject('My second list'));
 
-board.lists[0].cards.push(new CardObject('Get the shopping'))
-board.lists[0].cards.push(new CardObject('Learn to code'))
-board.lists[0].cards.push(new CardObject('Water the plants'))
+board.lists[0].cards.push(new CardObject('Get the shopping'));
+board.lists[0].cards.push(new CardObject('Learn to code'));
+board.lists[0].cards.push(new CardObject('Water the plants'));
+board.lists[0].cards.push(new CardObject('Make Dinner'));
+
+board.lists[1].cards.push(new CardObject('Recreate Trello'));
+board.lists[1].cards.push(new CardObject('Make the css beautiful'));
 
 module.exports = board;
