2ec295df0db2eb139b54ba56b11bdaae9cee07d9
bluerface <fallingsilkworm@gmail.com>
2016-11-23 11:12:15 +0000

    Add cards and lists to initial data

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,16 @@
+const ListObject = require('./ListObject');
+const CardObject = require('./CardObject');
 
 const board = {
   name: 'first board',
   lists: []
 };
+
+board.lists.push(new ListObject('My first list'));
+board.lists.push(new ListObject('My second list'));
+
+board.lists[0].cards.push(new CardObject('Get the shopping'))
+board.lists[0].cards.push(new CardObject('Learn to code'))
+board.lists[0].cards.push(new CardObject('Water the plants'))
 
 module.exports = board;
