b04a3bb2958a69aee18fc88c2cf9cc896095f935
bluerface <fallingsilkworm@gmail.com>
2016-11-23 13:51:09 +0000

    Add state updating to track field changes on move-menu

--- src/components/MoveMenu.js
+++ src/components/MoveMenu.js
@@ -2,20 +2,26 @@ const React = require('react');
 const enhanceWithClickOutside = require('react-click-outside');
 
 const MoveMenu = React.createClass({
+  getInitialState: function () {
+    return {
+      targetList: this.props.listIndex,
+      targetPosition: this.props.cardIndex
+    };
+  },
   render: function () {
     return (
       <div className='our-card move-menu highlighted'>
         <h1> Move Card </h1>
-        <p>Move to Board:</p>
-        <select className='select'>
+        <p>Move to List:</p>
+        <select className='select' name='targetList' onChange={this.handleChange}>
           {
             this.props.lists.map(function (list, i) {
-              return (<option key={i} value={list.name}>{list.name}</option>);
+              return (<option key={i} value={i}>{list.name}</option>);
             })
           }
         </select>
         <p> Move to position </p>
-        <select className='select'>
+        <select className='select' name='targetPosition' onChange={this.handleChange}>
           {
             this.props.cards.map(function (_, i) {
               return (<option key={i} value={i}>{i}</option>);
@@ -23,12 +29,19 @@ const MoveMenu = React.createClass({
           }
         </select>
         <br />
-        <button style={{marginTop: '5px'}} type='submit' className='button is-success'>Save</button>
+        <button type='submit' className='button is-success'>Save</button>
       </div>
     );
   },
   handleClickOutside: function () {
     this.props.toggleMenu();
+  },
+  handleChange: function (event) {
+    let newState = {};
+    newState[event.target.name] = event.target.value;
+    this.setState(newState, () => {
+      console.log(this.state);
+    });
   }
 });
 
